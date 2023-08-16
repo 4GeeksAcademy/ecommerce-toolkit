90e396d654cfaeaf5a3648845e7741108fbe6ddc
RoAlfonsin <130806097+RoAlfonsin@users.noreply.github.com>
2023-08-16 22:51:08 +0000

    demo and singles deleted, navbar created, router updated

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,15 +3,34 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+		<nav className="navbar navbar-expand-lg bg-primary-subtle">
+			<div className="container-fluid">
+				<Link className="navbar-brand ms-3" to={"/"}>Home</Link>
+				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNav">
+					<ul className="navbar-nav">
+						<li className="nav-item">
+							<Link className="nav-link" to={"/category"}>Shop By Category</Link>
+						</li>
+						<li className="nav-item">
+							<Link className="nav-link" to={"/wishlist"}>Wish List</Link>
+						</li>
+						<li className="nav-item">
+							<Link className="nav-link" to={"/search"}>Search</Link>
+						</li>
+						<li className="nav-item">
+							<Link className="nav-link" to={"/cart"}>Shopping Cart</Link>
+						</li>
+						<li className="nav-item">
+							<Link className="nav-link text-success" to={"/signin"}>Sign In</Link>
+						</li>
+						<li className="nav-item">
+							<Link className="nav-link text-danger" to={"/signout"}>Sign Out</Link>
+						</li>
+
+					</ul>
 				</div>
 			</div>
 		</nav>
