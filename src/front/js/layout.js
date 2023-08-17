895d2e7bbcde234d07bdfc68c9f6b1ebfd05b1c9
RoAlfonsin <130806097+RoAlfonsin@users.noreply.github.com>
2023-08-17 03:07:43 +0000

    created new item wizard

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -6,6 +6,8 @@ import { BackendURL } from "./component/backendURL";
 import { Home } from "./pages/home";
 import { Signin } from "./pages/signin.jsx";
 import { CreateAccount } from "./pages/createAccount.jsx";
+
+import { CreateItem } from "./pages/adminPages/itemWizard.jsx";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -17,7 +19,7 @@ const Layout = () => {
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+    if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL />;
 
     return (
         <div>
@@ -34,6 +36,7 @@ const Layout = () => {
                         <Route element={<h1>This is shopingcart</h1>} path="/cart" />
                         <Route element={<CreateAccount />} path="/createaccount" />
                         <Route element={<h1>Not found!</h1>} />
+                        <Route element={<CreateItem />} path="admin/item/wizard" />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
