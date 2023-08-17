67a6bea2779300d510225e32266af5198b2d2ed6
Verónica Jiménez <128405596+VeronicaJimenez@users.noreply.github.com>
2023-08-17 22:22:43 +0000

    Checkout view created, payment and delivery form ready. Still working on orden summary

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -6,6 +6,7 @@ import { BackendURL } from "./component/backendURL";
 import { Home } from "./pages/home";
 import { Signin } from "./pages/signin.jsx";
 import { CreateAccount } from "./pages/createAccount.jsx";
+import { Checkout } from "./pages/checkout.jsx";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -32,6 +33,7 @@ const Layout = () => {
                         <Route element={<Signin />} path="/signin" />
                         <Route element={<h1>This is Sign Out</h1>} path="/signout" />
                         <Route element={<h1>This is shopingcart</h1>} path="/cart" />
+                        <Route element={<Checkout/>} path="/checkout" />
                         <Route element={<CreateAccount />} path="/createaccount" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
