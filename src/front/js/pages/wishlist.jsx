3cb615360518936b08891bfacc6c739fe6d881ba
RoAlfonsin <130806097+RoAlfonsin@users.noreply.github.com>
2023-08-26 13:58:21 +0000

    not finnished

--- src/front/js/pages/wishlist.jsx
+++ src/front/js/pages/wishlist.jsx
@@ -1,90 +1,124 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
 export const Wishlist = () => {
+    const [stockItems, setStockItems] = useState([]);
+    const [outOfStockItems, setOutOfStockItems] = useState([]);
+    const { store, actions } = useContext(Context);
+    const user = store.user;
+
+    useEffect(() => {
+        fetchWishlist();
+    }, []);
+
+    const fetchWishlist = async () => {
+        let url = process.env.BACKEND_URL + "api/getwishlist/" + user;
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                //get items info using wishlist
+                url = process.env.BACKEND_URL + "api/items";
+                fetch(url)
+                    .then((response) => response.json())
+                    .then((data2) => {
+                        for (let i = 0; i < data.length; i++) {
+                            for (let j = 0; j < data2.length; j++) {
+                                if (data[i].item_id == data2[j].id) {
+                                    if (data2[j].stock > 0) {
+                                        setStockItems(stockItems => [...stockItems, data2[j]]);
+                                    } else {
+                                        setOutOfStockItems(outOfStockItems => [...outOfStockItems, data2[j]]);
+                                    }
+                                }
+                            }
+                        }
+                    })
+            });
+    };
+
+
+
     const handleAddToCart = (event) => {
-        
-      // TODO: Handle add to cart
+        //add to cart
+        const item_id = event.target.id;
+        const user_id = user;
+    };
+
+    const handleRemoveFromWishlist = (event) => {
+
     };
 
 
     return (
-        <div>
-            <div className="container">
-                <h2 className="my-3">Wish List</h2>
-                {/* In Stock */}
-                <div className="mt-5">
+        <div className="container">
+            <h2 className="my-3">Wish List</h2>
+            {/* Map elements in stock */}
+            {stockItems.map((item) => (
+                <div className="mt-5" key={item.id}>
                     <div className="row mx-2 mb-2 align-items-center">
                         <div className="col-md-3 text-center">
-                            <Link class="text-dark link-underline link-underline-opacity-0" to="/product/theid">
-                                <img className="img-fluid" src="//c1.staticflickr.com/1/466/19681864394_c332ae87df_t.jpg" />
+                            <Link className="text-dark link-underline link-underline-opacity-0" to={"/product/" + item.id}>
+                                <img className="img-thumbnail" src={item.image_url} width={"100px"} />
                             </Link>
-                            
                         </div>
                         <div className="col-md-9">
                             <div className="row mt-3 align-items-center">
                                 <div className="col-md-4">
                                     <div className="col-md-12 text-wrap">
-                                        <Link class="text-dark link-underline link-underline-opacity-0" to="/product/theid">Product name</Link>
+                                        <Link className="text-dark link-underline link-underline-opacity-0" to={"/product/" + item.id}>{item.name}</Link>
                                     </div>
-                                    <div className="col-12 text-primary text-opacity-50"><h6><span>$</span>00.00</h6></div>
+                                    <div className="col-12 text-primary text-opacity-50"><h6><span>$</span>{item.price}</h6></div>
                                 </div>
                                 <div className="col-md-3 px-5">
                                     <div className="text-secondary" >
                                         In Stock
-
                                     </div>
                                 </div>
                                 <div className="col-md-3 px-5">
-                                    <button type="button" className="btn bg-primary-subtle" onClick={(e) => handleAddToCart()}>Add to cart</button>
+                                    <button type="button" id={item.id} className="btn bg-primary-subtle" onClick={handleAddToCart}>Add to cart</button>
                                 </div>
                                 <div className="col-md-2 text-center">
-                                    <button type="button" className="btn btn-outline ">
-                                        <i className="fa-solid fa-trash fa-xl"></i>
+                                    <button type="button" className="btn btn-outline">
+                                        <i className="fa-solid fa-trash fa-xl" onClick={handleRemoveFromWishlist}></i>
                                     </button>
                                 </div>
                             </div>
                         </div>
-                        
                     </div>
-                
                 </div>
-                {/* Out of Stock */}
-                <div className="mt-5">
+            ))}
+            {/* Map elements out of stock */}
+            {outOfStockItems.map((item) => (
+                <div className="mt-5" key={item.id}>
                     <div className="row mx-2 mb-2 align-items-center">
                         <div className="col-md-3 text-center">
-                            <img className="img-fluid" src="//c1.staticflickr.com/1/466/19681864394_c332ae87df_t.jpg" />
+                            <img className="img-thumbnail" src={item.image_url} width={"100px"} />
                         </div>
                         <div className="col-md-9">
                             <div className="row mt-3 align-items-center">
                                 <div className="col-md-4">
-                                    <div className="col-md-12 text-wrap">Product name xxxxxxxxxxxxxxxxxx xxxxxxxxxx xxxxxxxxxx</div>
-                                    <div className="col-12 text-primary text-opacity-50"><h6><span>$</span>00.00</h6></div>
+                                    <div className="col-md-12 text-wrap">{item.name}</div>
+                                    <div className="col-12 text-primary text-opacity-50"><h6><span>$</span>{item.price}</h6></div>
                                 </div>
                                 <div className="col-md-3 px-5">
                                     <div className="text-danger text-opacity-50" >
                                         Out of Stock
-
                                     </div>
                                 </div>
                                 <div className="col-md-3 px-5">
-                                    <button type="button" className="btn bg-primary-subtle" onClick={(e) => setPassword()} disabled>Add to cart</button>
+                                    <button type="button" className="btn bg-primary-subtle" disabled>Add to cart</button>
                                 </div>
                                 <div className="col-md-2 text-center">
-                                    <button type="button" className="btn btn-outline" >
+                                    <button type="button" className="btn btn-outline" onClick={handleRemoveFromWishlist}>
                                         <i className="fa-solid fa-trash fa-xl"></i>
                                     </button>
                                 </div>
                             </div>
                         </div>
-                        
                     </div>
-                
                 </div>
-            </div>
-            
+            ))}
         </div>
-        
-
     );
 };
